831f52485fa4f8f11e3f1552809cf05d53d63a81
AJ@Y SHAHU <ajayshahu1104@gmail.com>
2025-10-08 12:26:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-0360c520857e3138f "
 instance_type = "t2.medium"
-key_name = "demoproject1"
+key_name = "insta"
 vpc_security_group_ids = ["sg-0777ec6755223e598"]
 tags = {
 Name = var.instance_names[count.index]
